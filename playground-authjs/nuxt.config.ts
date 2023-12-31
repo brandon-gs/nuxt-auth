7ecab8d5122af9ae5d52dc2069347ab7b1c63490
Brandon Garcia <cosmoscrew1@gmail.com>
2023-12-31 01:20:40 -0600

    testing failed

--- playground-authjs/nuxt.config.ts
+++ playground-authjs/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
     globalAppMiddleware: {
       isEnabled: true,
     },
-    baseURL: "http://localhost:3001",
+    baseURL: "http://localhost:3000",
   },
   routeRules: {
     "/with-caching": {
